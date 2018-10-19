5b5c4b2060bfb22ffcc0e01ba51e8f0ebd216586
forever123 <liaojialin@juran.com.cn>
2018-10-19 16:42:17 +0800

    第二版
    新增swagger配置

--- bbyx_parent/bgy_bbyx_controller/bgy_bbyx_pc_interface/src/main/java/com/pc_interface/controller/DemoController.java
+++ bbyx_parent/bgy_bbyx_controller/bgy_bbyx_pc_interface/src/main/java/com/pc_interface/controller/DemoController.java
@@ -3,19 +3,35 @@ package com.pc_interface.controller;
 import java.util.List;
 
 import org.bgy_bbyx_model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.DemoService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value = "DemoController", description = "测试controller")
 public class DemoController {
 	@Autowired
 	private DemoService demoService;
+	/**
+	 * 引入日志，注意都是"org.slf4j"包下
+	 */
+	private final static Logger logger = LoggerFactory.getLogger(DemoController.class);
 	
 	@RequestMapping("/demo")
+	@ApiOperation(value = "测试方法", notes = "模拟用户访问",httpMethod ="GET")
+	@ApiImplicitParam(name="user", value="User", required = true, dataType = "User")
 	public List<User> demo(User user) {
-		return demoService.demo(user);
+		List<User> demos = demoService.demo(user);
+		logger.info("访问selectUser"+demos.toString());
+		return demos;
 	}
 }
--- bbyx_parent/bgy_bbyx_model/src/main/java/org/bgy_bbyx_model/User.java
+++ bbyx_parent/bgy_bbyx_model/src/main/java/org/bgy_bbyx_model/User.java
@@ -1,7 +1,13 @@
 package org.bgy_bbyx_model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("用户")
 public class User {
+	@ApiModelProperty(value="用户名")
 	private String userName;
+	@ApiModelProperty(value="密码")
 	private String password;
 	public String getUserName() {
 		return userName;
